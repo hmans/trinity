d58b519a3cc14a8c7aa23d4ee93d2530bb51803c
Hendrik Mans <hendrik@mans.de>
2022-03-16 17:16:43 +0100

    Context

--- packages/playground/src/trinity/Engine.tsx
+++ packages/playground/src/trinity/Engine.tsx
@@ -1,41 +1,55 @@
-import { useRef, useEffect } from "react";
-import * as THREE from "three";
+import { useRef, useEffect, createContext } from "react"
+import * as THREE from "three"
+import { useConst } from "./util/useConst"
+
+const SceneObjectContext = createContext<THREE.Object3D>(null!)
 
 export function Engine() {
-  const canvas = useRef<HTMLCanvasElement>(null!);
+  const canvas = useRef<HTMLCanvasElement>(null!)
+  const scene = useConst(() => new THREE.Scene())
 
   useEffect(() => {
-    const el = canvas.current;
+    const el = canvas.current
 
-    const scene = new THREE.Scene();
-    const camera = new THREE.PerspectiveCamera(
-      75,
-      el.clientWidth / el.clientHeight,
-      0.1,
-      1000
-    );
-    camera.position.z = 10;
+    /* Renderer */
+    const renderer = new THREE.WebGLRenderer({ canvas: el })
+    renderer.setSize(el.clientWidth, el.clientHeight)
 
-    const renderer = new THREE.WebGLRenderer({ canvas: el });
-    renderer.setSize(el.clientWidth, el.clientHeight);
+    /* Camera */
+    const camera = new THREE.PerspectiveCamera(75, el.clientWidth / el.clientHeight, 0.1, 1000)
+    camera.position.z = 10
 
-    scene.add(new THREE.AmbientLight());
+    /* Lights */
+    scene.add(new THREE.AmbientLight())
 
-    const cube = new THREE.Mesh();
-    cube.geometry = new THREE.DodecahedronGeometry();
-    cube.material = new THREE.MeshStandardMaterial({ color: "hotpink" });
-    scene.add(cube);
+    /* Cube */
+    const cube = new THREE.Mesh()
+    cube.geometry = new THREE.DodecahedronGeometry()
+    cube.material = new THREE.MeshStandardMaterial({ color: "hotpink" })
+    cube.position.x = -5
+    scene.add(cube)
 
+    /* Render loop */
+    let looping = true
     const tick = () => {
-      cube.rotation.x = cube.rotation.y += 0.01;
+      cube.rotation.x = cube.rotation.y += 0.01
 
       /* Render */
-      renderer.render(scene, camera);
-      requestAnimationFrame(tick);
-    };
+      renderer.render(scene, camera)
+      if (looping) requestAnimationFrame(tick)
+    }
 
-    tick();
-  }, []);
+    tick()
 
-  return <canvas ref={canvas}></canvas>;
+    return () => {
+      looping = false
+      scene.clear()
+    }
+  }, [])
+
+  return (
+    <canvas ref={canvas}>
+      <SceneObjectContext.Provider value={scene}></SceneObjectContext.Provider>
+    </canvas>
+  )
 }
---
+++ packages/playground/src/trinity/util/useConst.ts
@@ -0,0 +1,9 @@
+import { useRef } from "react"
+
+export function useConst<T>(fn: () => T) {
+  const ref = useRef<T>()
+
+  if (!ref.current) ref.current = fn()
+
+  return ref.current
+}
