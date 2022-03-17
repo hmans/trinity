3adaab22b436188fe22067d80ec047afcc916519
Hendrik Mans <hendrik@mans.de>
2022-03-17 15:51:52 +0100

    Export useAnimationFrame

--- packages/trinity/src/index.ts
+++ packages/trinity/src/index.ts
@@ -1,5 +1,5 @@
 export * from "./reactor/types"
 export { Engine } from "./engine/Engine"
+export { useAnimationFrame } from "./engine/useAnimationFrame"
 import { Reactor } from "./reactor/Reactor"
-
 export default Reactor
