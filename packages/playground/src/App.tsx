6cd4757635de94665580b569b73cba6aa9e66483
Hendrik Mans <hendrik@mans.de>
2022-03-17 16:24:09 +0100

    Woohoo

--- packages/playground/src/App.tsx
+++ packages/playground/src/App.tsx
@@ -2,14 +2,14 @@ import { useRef } from "react"
 import { Mesh } from "three"
 import T from "@hmans/trinity"
 import { Engine } from "@hmans/trinity"
-// import { useAnimationFrame } from "@hmans/trinity"
+import { useAnimationFrame } from "@hmans/trinity"
 
 function Thingy() {
   const mesh = useRef<Mesh>(null!)
 
-  // useAnimationFrame(() => {
-  //   mesh.current.rotation.x = mesh.current.rotation.y += 0.01
-  // })
+  useAnimationFrame(() => {
+    mesh.current.rotation.x = mesh.current.rotation.y += 0.01
+  })
 
   return (
     <T.Mesh ref={mesh} position={[1, 2, 3]}>
