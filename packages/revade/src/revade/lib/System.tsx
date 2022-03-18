e2da0b9d380beeb7d8a0af65156fc336480563e5
Hendrik Mans <hendrik@mans.de>
2022-03-18 23:42:22 +0100

    Support stage in System

--- packages/revade/src/revade/lib/System.tsx
+++ packages/revade/src/revade/lib/System.tsx
@@ -1,17 +1,20 @@
 import { Callback } from "@hmans/trinity"
+import { TickerStage } from "@hmans/trinity/src/engine/Ticker"
 import { QueriedEntity, Query } from "miniplex"
 import { ECS, Entity } from "../state"
 
 const System = <Q extends Query<Entity>>({
   archetype,
+  stage = "update",
   children
 }: {
   archetype: Q
+  stage?: TickerStage
   children: (entities: QueriedEntity<Entity, Q>[], dt: number) => void
 }) => {
   const { entities } = ECS.world.archetype(...archetype)
 
-  return <Callback>{(dt: number) => children(entities, dt)}</Callback>
+  return <Callback stage={stage}>{(dt: number) => children(entities, dt)}</Callback>
 }
 
 export default System
