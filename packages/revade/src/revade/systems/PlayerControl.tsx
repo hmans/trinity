42c97718bfbf3986d17d64aa31e13a92d6011ee0
Hendrik Mans <hendrik@mans.de>
2022-03-19 19:30:26 +0100

    Cleanup

--- packages/revade/src/revade/systems/PlayerControl.tsx
+++ packages/revade/src/revade/systems/PlayerControl.tsx
@@ -1,16 +1,15 @@
 import { VectorControl } from "@hmans/controlfreak"
 import { Vec2 } from "planck"
 import System from "../lib/System"
-import { tmpVector3 } from "../lib/temps"
 
-export const PlayerControl = ({ thrust = 100 }) => (
+export const PlayerControl = ({ thrust = 40 }) => (
   <System archetype={["controller", "body"]}>
     {(entities, dt) => {
       for (const { controller, body } of entities) {
         controller.update()
 
         const move = controller.controls.move as VectorControl
-        body.applyForceToCenter(Vec2(move.value).mul(40))
+        body.applyForceToCenter(Vec2(move.value).mul(thrust))
       }
     }}
   </System>
