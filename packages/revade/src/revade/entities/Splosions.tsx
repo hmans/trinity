989864dbd7f99cf1027fd934988770d26bb89694
Hendrik Mans <hendrik@mans.de>
2022-03-20 21:23:00 +0100

    Add a simple splosion animation thanks to popmotion

--- packages/revade/src/revade/entities/Splosions.tsx
+++ packages/revade/src/revade/entities/Splosions.tsx
@@ -1,6 +1,8 @@
 import { makeInstanceComponents } from "@hmans/trinity"
 import { ECS } from "../state"
 import T from "@hmans/trinity"
+import { useEffect } from "react"
+import { animate, easeIn, easeInOut, easeOut } from "popmotion"
 
 const Splosion = makeInstanceComponents()
 
@@ -12,23 +14,34 @@ export const Splosions = () => (
     </Splosion.Root>
 
     <ECS.Collection tag="splosion" initial={1} memoize>
-      {(entity) => (
-        <>
-          <ECS.Component name="transform">
-            <T.Group position-x={-20} scale={10}>
-              <Splosion.Instance />
-            </T.Group>
-          </ECS.Component>
+      {(entity) => {
+        useEffect(() => {
+          animate({
+            to: [10, 15, 0],
+            ease: [easeOut, easeIn],
+            offset: [0, 0.1, 1],
+            duration: 2000,
+            onUpdate: (latest) => entity.transform?.scale.setScalar(latest)
+          })
+        }, [])
+        return (
+          <>
+            <ECS.Component name="transform">
+              <T.Group position-x={-20} scale={10}>
+                <Splosion.Instance />
+              </T.Group>
+            </ECS.Component>
 
-          <ECS.Component
-            name="auto"
-            data={{
-              delay: 2,
-              callback: () => ECS.world.queue.destroyEntity(entity)
-            }}
-          />
-        </>
-      )}
+            <ECS.Component
+              name="auto"
+              data={{
+                delay: 2,
+                callback: () => ECS.world.queue.destroyEntity(entity)
+              }}
+            />
+          </>
+        )
+      }}
     </ECS.Collection>
   </>
 )
