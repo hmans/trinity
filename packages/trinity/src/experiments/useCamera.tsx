4cc01e6efe76391047c70288d2381e7bb14139a2
Hendrik Mans <hendrik@mans.de>
2022-03-20 12:52:31 +0100

    camera lookat origin

--- packages/trinity/src/experiments/useCamera.tsx
+++ packages/trinity/src/experiments/useCamera.tsx
@@ -6,7 +6,10 @@ export const useCamera = <T extends Camera = Camera>() => {
   const ref = useRef<T>(null!)
   const view = useView()
 
-  useEffect(() => view.setCamera(ref.current), [ref.current])
+  useEffect(() => {
+    ref.current.lookAt(0, 0, 0)
+    view.setCamera(ref.current)
+  }, [ref.current])
 
   return ref
 }
