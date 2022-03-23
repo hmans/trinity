ca37aa08c2f99afbfccfff9f8faec4a9c4c96b5a
Hendrik Mans <hendrik@mans.de>
2022-03-23 16:21:04 +0100

    Don't spawn enemies too close to the player

--- packages/revade/src/revade/systems/EnemySpawner.tsx
+++ packages/revade/src/revade/systems/EnemySpawner.tsx
@@ -9,10 +9,8 @@ const players = ECS.world.archetype("player").entities
 
 const tmpVec3 = new Vector3()
 
-const getSpawnPosition = () => {
-  tmpVec3.set(plusMinus(55), plusMinus(55), 0)
-
-  return tmpVec3.clone()
+const getSpawnPosition = (vec3: Vector3) => {
+  vec3.set(plusMinus(55), plusMinus(55), 0)
 }
 
 export const EnemySpawner = ({ interval = 0.5 }) => {
@@ -20,10 +18,16 @@ export const EnemySpawner = ({ interval = 0.5 }) => {
 
   useTicker("update", (dt) => {
     cooldown.current -= dt
+
     if (cooldown.current <= 0) {
       cooldown.current += interval
 
-      ECS.world.createEntity({ enemy: Tag, spawnAt: getSpawnPosition() })
+      getSpawnPosition(tmpVec3)
+      while (tmpVec3.distanceTo(players[0].transform!.position) < 50) {
+        getSpawnPosition(tmpVec3)
+      }
+
+      ECS.world.createEntity({ enemy: Tag, spawnAt: tmpVec3 })
     }
   })
 
