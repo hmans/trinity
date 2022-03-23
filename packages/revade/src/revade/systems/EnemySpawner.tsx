e3a39bb4de99e03d2a1d2a9776d88bd16d03ade0
Hendrik Mans <hendrik@mans.de>
2022-03-23 16:24:48 +0100

    Tweak spawn frequency and amount

--- packages/revade/src/revade/systems/EnemySpawner.tsx
+++ packages/revade/src/revade/systems/EnemySpawner.tsx
@@ -1,6 +1,6 @@
 import { useTicker } from "@hmans/trinity"
 import { Tag } from "miniplex"
-import { plusMinus } from "randomish"
+import { plusMinus, number } from "randomish"
 import { useRef } from "react"
 import { Vector3 } from "three"
 import { ECS } from "../state"
@@ -8,12 +8,13 @@ import { ECS } from "../state"
 const players = ECS.world.archetype("player").entities
 
 const tmpVec3 = new Vector3()
+const position = new Vector3()
 
 const getSpawnPosition = (vec3: Vector3) => {
   vec3.set(plusMinus(55), plusMinus(55), 0)
 }
 
-export const EnemySpawner = ({ interval = 0.5 }) => {
+export const EnemySpawner = ({ interval = 2, amount = 5 }) => {
   const cooldown = useRef(interval)
 
   useTicker("update", (dt) => {
@@ -22,12 +23,21 @@ export const EnemySpawner = ({ interval = 0.5 }) => {
     if (cooldown.current <= 0) {
       cooldown.current += interval
 
-      getSpawnPosition(tmpVec3)
-      while (tmpVec3.distanceTo(players[0].transform!.position) < 50) {
-        getSpawnPosition(tmpVec3)
+      getSpawnPosition(position)
+      while (position.distanceTo(players[0].transform!.position) < 50) {
+        getSpawnPosition(position)
       }
 
-      ECS.world.createEntity({ enemy: Tag, spawnAt: tmpVec3 })
+      for (let i = 0; i < amount; i++) {
+        ECS.world.createEntity({
+          enemy: Tag,
+          spawnAt: tmpVec3
+            .randomDirection()
+            .multiplyScalar(number(5))
+            .add(position)
+            .clone()
+        })
+      }
     }
   })
 
