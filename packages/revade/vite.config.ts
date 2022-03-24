b49acbb5065fc0f7a7d026806592ec8e80ad2be1
Hendrik Mans <hendrik@mans.de>
2022-03-24 10:04:28 +0100

    Add rollup visualizer

--- packages/revade/vite.config.ts
+++ packages/revade/vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from "vite"
 import react from "@vitejs/plugin-react"
+import { visualizer } from "rollup-plugin-visualizer"
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -7,5 +8,15 @@ export default defineConfig({
   optimizeDeps: {
     exclude: ["miniplex", "@hmans/controlfreak"],
     include: ["react/jsx-runtime"]
+  },
+  build: {
+    rollupOptions: {
+      plugins: [
+        visualizer({
+          filename: "stats.html",
+          template: "treemap"
+        })
+      ]
+    }
   }
 })
