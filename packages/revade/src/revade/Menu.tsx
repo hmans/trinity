5c2bd230f3bbe209836d4e4129b0655e72d894a4
Hendrik Mans <hendrik@mans.de>
2022-03-27 20:28:10 +0200

    Add a little delay to the menu

--- packages/revade/src/revade/Menu.tsx
+++ packages/revade/src/revade/Menu.tsx
@@ -1,16 +1,21 @@
 import T, { useTicker } from "@hmans/trinity"
-import React, { useEffect, useRef } from "react"
+import React, { useEffect, useRef, useState } from "react"
 import { Mesh } from "three"
 import { controller } from "./controller"
 import { GameFSM } from "./Game"
 
 const MenuOrb = ({ speed = 1.5 }) => {
   const ref = useRef<Mesh>(null!)
+  const [ready, setReady] = useState(false)
+
+  useEffect(() => {
+    setTimeout(() => setReady(true), 500)
+  }, [])
 
   useTicker("update", (dt) => {
     ref.current.rotation.x = ref.current.rotation.y += speed * dt
 
-    if (controller.controls.fire.value) {
+    if (ready && controller.controls.fire.value) {
       GameFSM.transition("startGame")
     }
   })
