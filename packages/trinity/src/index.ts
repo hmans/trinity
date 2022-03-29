29228c47b7e389ccc4609021a2c5aa64f04a88c6
Hendrik Mans <hendrik@mans.de>
2022-03-29 16:54:12 +0200

    Remove Instances and Text from old package

--- packages/trinity/src/index.ts
+++ packages/trinity/src/index.ts
@@ -16,9 +16,7 @@ export { useTicker } from "./engine/Ticker"
 export { useParent } from "./engine/useParent"
 
 /* Experiments */
-export { makeInstanceComponents } from "./tools/instances"
 export { useCamera } from "./experiments/useCamera"
-export { Text } from "./toolbox/Text"
 
 /* Export the Reactor as our default export */
 import { Reactor } from "./reactor/Reactor"
