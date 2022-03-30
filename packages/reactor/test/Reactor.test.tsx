b63c832f424dbc47445374dd3875af9496afae7d
Hendrik Mans <hendrik@mans.de>
2022-03-30 18:25:50 +0200

    What

--- packages/reactor/test/Reactor.test.tsx
+++ packages/reactor/test/Reactor.test.tsx
@@ -14,29 +14,29 @@ describe("Reactor", () => {
   it("uses the props to set attributes on the managed THREE object", () => {
     const ref = createRef<Mesh>()
     render(<T.Mesh ref={ref} position-x={5} />)
-    expect(ref.current.position.x).toEqual(5)
+    expect(ref.current!.position.x).toEqual(5)
   })
 
   it("provides shortcut props for Vector3 attributes", () => {
     const ref = createRef<Mesh>()
     render(<T.Mesh ref={ref} position={[1, 2, 3]} />)
-    expect(ref.current.position.x).toEqual(1)
-    expect(ref.current.position.y).toEqual(2)
-    expect(ref.current.position.z).toEqual(3)
+    expect(ref.current!.position.x).toEqual(1)
+    expect(ref.current!.position.y).toEqual(2)
+    expect(ref.current!.position.z).toEqual(3)
   })
 
   it("provides shortcut props for attributes that have a .setScalar method", () => {
     const ref = createRef<Mesh>()
     render(<T.Mesh ref={ref} scale={10} />)
-    expect(ref.current.scale.x).toEqual(10)
-    expect(ref.current.scale.y).toEqual(10)
-    expect(ref.current.scale.z).toEqual(10)
+    expect(ref.current!.scale.x).toEqual(10)
+    expect(ref.current!.scale.y).toEqual(10)
+    expect(ref.current!.scale.z).toEqual(10)
   })
 
   it("provides shortcut props for color attributes", () => {
     const ref = createRef<MeshStandardMaterial>()
     render(<T.MeshStandardMaterial ref={ref} color="red" />)
-    expect(ref.current.color.getHexString()).toEqual("ff0000")
+    expect(ref.current!.color.getHexString()).toEqual("ff0000")
   })
 
   it("allows you to attach objects to other objects through the 'attach' prop", () => {
@@ -53,7 +53,7 @@ describe("Reactor", () => {
       </T.Mesh>
     )
 
-    expect(mesh.current.material).toBe(material.current)
+    expect(mesh.current!.material).toBe(material.current)
   })
 
   it("automatically attaches materials and geometries", () => {
@@ -68,8 +68,8 @@ describe("Reactor", () => {
       </T.Mesh>
     )
 
-    expect(mesh.current.material).toBe(material.current)
-    expect(mesh.current.geometry).toBe(geometry.current)
+    expect(mesh.current!.material).toBe(material.current)
+    expect(mesh.current!.geometry).toBe(geometry.current)
   })
 
   it("automatically attaches the object to a parent if there is one", () => {
@@ -82,6 +82,6 @@ describe("Reactor", () => {
       </T.Mesh>
     )
 
-    expect(child.current.parent).toBe(parent.current)
+    expect(child.current!.parent).toBe(parent.current)
   })
 })
