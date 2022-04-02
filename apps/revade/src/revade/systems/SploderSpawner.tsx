898f761ff2a9f870be3329f5a544577180b83842
Hendrik Mans <hendrik@mans.de>
2022-04-02 21:39:16 +0200

    Fix sploder positioning

--- apps/revade/src/revade/systems/SploderSpawner.tsx
+++ apps/revade/src/revade/systems/SploderSpawner.tsx
@@ -5,17 +5,24 @@ import { useRef } from "react"
 import { Vector3 } from "three"
 import { ECS } from "../state"
 
+const levels = ECS.world.archetype("level", "transform").entities
+
 export const SploderSpawner = ({ interval = 3 }) => {
   const cooldown = useRef(interval)
 
   useTicker("update", (dt) => {
+    const level = levels[0]
+    if (!level) return
+
     cooldown.current -= dt
     if (cooldown.current <= 0) {
       cooldown.current += interval
 
       ECS.world.createEntity({
         sploder: Tag,
-        spawnAt: new Vector3(plusMinus(50), plusMinus(50), 0)
+        spawnAt: new Vector3(plusMinus(50), plusMinus(50), 0).applyQuaternion(
+          level.transform.quaternion
+        )
       })
     }
   })
