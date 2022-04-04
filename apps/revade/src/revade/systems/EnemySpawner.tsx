351cdbfe0ef052b73e33252f7047563b707cecc5
Hendrik Mans <hendrik@mans.de>
2022-04-04 19:12:14 +0200

    Fix positions at which enemies spawn

--- apps/revade/src/revade/systems/EnemySpawner.tsx
+++ apps/revade/src/revade/systems/EnemySpawner.tsx
@@ -1,6 +1,6 @@
 import { Tag } from "miniplex"
 import { number, plusMinus } from "randomish"
-import { Vector3 } from "three"
+import { Quaternion, Vector3 } from "three"
 import ArchetypeSystem from "../../lib/ArchetypeSystem"
 import { ECS } from "../state"
 
@@ -10,8 +10,9 @@ const levels = ECS.world.archetype("level", "transform").entities
 const tmpVec3 = new Vector3()
 const position = new Vector3()
 
-const getSpawnPosition = (vec3: Vector3) => {
+const getSpawnPosition = (vec3: Vector3, rotation: Quaternion) => {
   vec3.set(plusMinus(50), plusMinus(50), 0)
+  vec3.applyQuaternion(rotation)
 }
 
 export const EnemySpawner = () => (
@@ -33,17 +34,14 @@ export const EnemySpawner = () => (
           enemySpawner.interval = Math.max(enemySpawner.interval - 0.1, 0.5)
 
           /* Find a position that's not too close to the player */
-          getSpawnPosition(position)
+          getSpawnPosition(position, level.transform.quaternion)
           while (
             players[0] &&
             players[0].transform &&
             position.distanceTo(players[0].transform.position) < 50
           ) {
-            getSpawnPosition(position)
+            getSpawnPosition(position, level.transform.quaternion)
           }
-
-          /* Rotate position according to level rotation */
-          position.applyQuaternion(level.transform.quaternion)
 
           /* Spawn a couple of enemies with a random distance around the position */
           for (let i = 0; i < enemySpawner.amount; i++) {
