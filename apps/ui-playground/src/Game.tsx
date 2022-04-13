986b783b697bdab62caf3ad845b9a9a5f1b1ef58
Hendrik Mans <hendrik@mans.de>
2022-04-13 18:51:46 +0200

    Switch to emotion

--- apps/ui-playground/src/Game.tsx
+++ apps/ui-playground/src/Game.tsx
@@ -1,1 +1,12 @@
-export const Game = () => <div />
+import styled from "@emotion/styled"
+
+export const FauxFullscreenGame = styled.div({
+  position: "fixed",
+  left: 0,
+  top: 0,
+  width: "100vw",
+  height: "100vh",
+  backgroundColor: "#555"
+})
+
+export const Game = () => <FauxFullscreenGame onClick={(e) => console.log(e)} />
--- packages/ui/src/Canvas.tsx
+++ packages/ui/src/Canvas.tsx
@@ -1,9 +1,11 @@
-import { styled } from "@stitches/react"
+import styled from "@emotion/styled"
 
-export const Canvas = styled("div", {
+export const Canvas = styled.div({
   position: "fixed",
   left: 0,
   top: 0,
   width: "100vw",
-  height: "100vh"
+  height: "100vh",
+  pointerEvents: "none",
+  zIndex: 100
 })
