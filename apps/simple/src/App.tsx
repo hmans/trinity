ca93077c1e4506e19a566abee06f23f3e5d047b9
Hendrik Mans <hendrik@mans.de>
2022-04-19 15:46:42 +0200

    Rotate thingy

--- apps/simple/src/App.tsx
+++ apps/simple/src/App.tsx
@@ -1,12 +1,28 @@
 import T, { Renderer, View } from "react-trinity"
-import { Ticker } from "react-trinity/ticker"
+import { Ticker, useTicker } from "react-trinity/ticker"
 import { useCamera } from "react-trinity/experiments"
-import { PerspectiveCamera } from "three"
+import { Mesh, PerspectiveCamera } from "three"
+import { useRef } from "react"
 
 const Camera = () => {
   const camera = useCamera<PerspectiveCamera>()
 
   return <T.PerspectiveCamera position={[0, 0, -10]} ref={camera} />
+}
+
+const Thingy = ({ speed = 0.5 }) => {
+  const ref = useRef<Mesh>(null!)
+
+  useTicker("update", (dt) => {
+    ref.current.rotation.x = ref.current.rotation.y += speed * dt
+  })
+
+  return (
+    <T.Mesh ref={ref}>
+      <T.DodecahedronGeometry />
+      <T.MeshStandardMaterial color="hotpink" />
+    </T.Mesh>
+  )
 }
 
 function App() {
@@ -15,12 +31,8 @@ function App() {
       <Renderer>
         <View>
           <Camera />
-
           <T.AmbientLight intensity={1} />
-          <T.Mesh>
-            <T.DodecahedronGeometry />
-            <T.MeshStandardMaterial color="hotpink" />
-          </T.Mesh>
+          <Thingy />
         </View>
       </Renderer>
     </Ticker>
