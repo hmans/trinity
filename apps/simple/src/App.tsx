463e364b04277c7322d08b9acc6ef066e6cab261
Hendrik Mans <hendrik@mans.de>
2022-04-20 17:34:43 +0200

    Nice!

--- apps/simple/src/App.tsx
+++ apps/simple/src/App.tsx
@@ -1,23 +1,23 @@
-import { EffectCallback, useCallback, useEffect, useRef, useState } from "react"
+import {
+  EffectCallback,
+  FC,
+  MutableRefObject,
+  useCallback,
+  useEffect,
+  useRef,
+  useState
+} from "react"
 import T, { Renderer } from "react-trinity"
 import { useRenderer } from "react-trinity/src/engine/Renderer"
 import { useWindowResizeHandler } from "react-trinity/src/engine/useWindowResizeHandler"
 import { useParent } from "react-trinity/src/reactor"
-import { Ticker, Update } from "react-trinity/ticker"
+import { Ticker, Update, useTicker } from "react-trinity/ticker"
 import * as THREE from "three"
 
 const AutoRotate = ({ speed = 1 }) => (
   <Update>
     {(dt, { parent }) => (parent.rotation.x = parent.rotation.y += speed * dt)}
   </Update>
-)
-
-const Thingy = () => (
-  <T.Mesh>
-    <T.DodecahedronGeometry />
-    <T.MeshStandardMaterial color="hotpink" />
-    <AutoRotate speed={1.5} />
-  </T.Mesh>
 )
 
 const OnWindowResize = <T extends any = any>(props: {
@@ -55,43 +55,68 @@ const EventHandling = () => {
   return null
 }
 
+const View: FC<{
+  scene: MutableRefObject<THREE.Scene>
+  camera: MutableRefObject<THREE.Camera>
+  render?: (state: {
+    scene: THREE.Scene
+    camera: THREE.Camera
+    renderer: THREE.WebGLRenderer
+  }) => void
+}> = ({ scene, camera, render }) => {
+  const renderer = useRenderer()
+
+  useTicker("render", () => {
+    render
+      ? render({ renderer, scene: scene.current, camera: camera.current })
+      : renderer.render(scene.current, camera.current)
+  })
+
+  useWindowResizeHandler(() => {
+    if (!camera.current) return
+
+    const width = window.innerWidth
+    const height = window.innerHeight
+
+    if (camera.current instanceof THREE.PerspectiveCamera) {
+      camera.current.aspect = width / height
+      camera.current.updateProjectionMatrix()
+    }
+  })
+
+  return null
+}
+
 const App = () => {
   const scene = useRef<THREE.Scene>(null!)
+  const camera = useRef<THREE.PerspectiveCamera>(null!)
 
   return (
     <Ticker>
       <Renderer>
+        {/* The view actually takes care of rendering, event handling, etc. */}
+
+        <View
+          scene={scene}
+          camera={camera}
+          render={({ renderer, scene, camera }) => {
+            renderer.render(scene, camera)
+            /* Or alternatively, go crazy with an effects composer here */
+          }}
+        />
+
         {/* The scene, with some objects, and a camera */}
         <T.Scene ref={scene}>
-          <T.PerspectiveCamera position={[0, 0, 10]}>
-            <EventHandling />
-
-            {/* Handle window resizing */}
-            <OnWindowResize>
-              {(camera: THREE.PerspectiveCamera) => {
-                const width = window.innerWidth
-                const height = window.innerHeight
-
-                camera.aspect = width / height
-                camera.updateProjectionMatrix()
-              }}
-            </OnWindowResize>
-
-            {/* Our actual rendering code */}
-            <Update stage="render">
-              {(_, { parent: camera, renderer }) =>
-                renderer.render(
-                  scene.current,
-                  camera as THREE.PerspectiveCamera
-                )
-              }
-            </Update>
-          </T.PerspectiveCamera>
+          <T.PerspectiveCamera position={[0, 0, 10]} ref={camera} />
 
           <T.AmbientLight intensity={0.2} />
           <T.DirectionalLight intensity={0.7} position={[10, 10, 10]} />
 
-          <Thingy />
+          <T.Mesh>
+            <T.DodecahedronGeometry />
+            <T.MeshStandardMaterial color="hotpink" />
+            <AutoRotate speed={1.5} />
+          </T.Mesh>
         </T.Scene>
       </Renderer>
     </Ticker>
