7858fc2e7327a410c165dcaade1700e6478cf428
Hendrik Mans <hendrik@mans.de>
2022-04-20 21:35:16 +0200

    Cleanup

--- packages/trinity/src/engine/View.tsx
+++ packages/trinity/src/engine/View.tsx
@@ -1,22 +1,9 @@
 import { useConst } from "@hmans/react-toolbox"
-import React, {
-  createContext,
-  FC,
-  MutableRefObject,
-  ReactNode,
-  useContext,
-  useEffect,
-  useLayoutEffect
-} from "react"
-import { Camera, PerspectiveCamera, Scene, TextureLoader, Vector2 } from "three"
+import React, { createContext, FC, ReactNode, useContext } from "react"
+import { Camera, PerspectiveCamera, Scene } from "three"
 import { EffectComposer } from "three/examples/jsm/postprocessing/EffectComposer"
 import { RenderPass } from "three/examples/jsm/postprocessing/RenderPass"
-import { ShaderPass } from "three/examples/jsm/postprocessing/ShaderPass"
-import { UnrealBloomPass } from "three/examples/jsm/postprocessing/UnrealBloomPass"
-import { VignetteShader } from "three/examples/jsm/shaders/VignetteShader"
-import { LensDirtShader } from "../postprocessing/shaders/LensDirtShader"
 import { EffectPass } from "../postprocessing"
-import { Constructor } from "../reactor"
 import { Update } from "../ticker"
 import { EventHandling } from "./EventHandling"
 import { OnWindowResize } from "./OnWindowResize"
@@ -33,23 +20,6 @@ export const View: FC<{
 }> = ({ scene, camera, children }) => {
   const renderer = useRenderer()
   const composer = useConst(() => new EffectComposer(renderer))
-
-  useEffect(() => {
-    if (!camera || !scene) return
-    // composer.addPass(new RenderPass(scene, camera))
-    // composer.addPass(new UnrealBloomPass(new Vector2(256, 256), 1.5, 0.8, 0.3))
-    // const dirt = new ShaderPass(LensDirtShader)
-    // dirt.uniforms["tDirt"].value = new TextureLoader().load(
-    //   "/textures/dirt01.png"
-    // )
-    // dirt.uniforms["strength"].value = 0.5
-    // composer.addPass(dirt)
-    // /* Vignette */
-    // const vignette = new ShaderPass(VignetteShader)
-    // vignette.uniforms["offset"].value = 0.5
-    // vignette.uniforms["darkness"].value = 2
-    // composer.addPass(vignette)
-  }, [composer, scene, camera])
 
   return (
     <ViewContext.Provider value={{ composer }}>
