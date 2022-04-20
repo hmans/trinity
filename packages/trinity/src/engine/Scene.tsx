825466287044905bd81be9ee63665591f42b6b4c
Hendrik Mans <hendrik@mans.de>
2022-04-20 08:53:30 +0200

    Scene forwardRef

--- packages/trinity/src/engine/Scene.tsx
+++ packages/trinity/src/engine/Scene.tsx
@@ -1,4 +1,4 @@
-import React, { createContext, FC, ReactNode, useContext } from "react"
+import React, { createContext, forwardRef, useContext } from "react"
 import * as THREE from "three"
 import T, { ReactorComponentProps, useManagedThreeObject } from "../reactor"
 
@@ -6,14 +6,14 @@ type SceneProps = ReactorComponentProps<typeof THREE.Scene>
 
 const SceneContext = createContext<THREE.Scene>(null!)
 
-export const Scene: FC<SceneProps> = (sceneProps) => {
+export const Scene = forwardRef<THREE.Scene, SceneProps>((sceneProps, ref) => {
   const scene = useManagedThreeObject(() => new THREE.Scene())
 
   return (
     <SceneContext.Provider value={scene}>
-      <T.Scene object={scene} {...sceneProps} />
+      <T.Scene object={scene} ref={ref} {...sceneProps} />
     </SceneContext.Provider>
   )
-}
+})
 
 export const useScene = () => useContext(SceneContext)
