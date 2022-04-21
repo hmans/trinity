d726f732f9e10b82eb7ff15a69545f614ff933c7
Hendrik Mans <hendrik@mans.de>
2022-04-21 16:09:19 +0200

    Use <Application>

--- apps/space/src/Game.tsx
+++ apps/space/src/Game.tsx
@@ -1,10 +1,9 @@
 import { useState } from "react"
 import T, {
+  Application,
   Composer,
   EventHandling,
-  OnWindowResize,
-  Renderer,
-  Ticker
+  OnWindowResize
 } from "react-trinity"
 import { RenderPass } from "react-trinity/postprocessing"
 import * as THREE from "three"
@@ -14,45 +13,43 @@ export const Game = () => {
   const [camera, setCamera] = useState<THREE.PerspectiveCamera | null>()
 
   return (
-    <Ticker>
-      <Renderer>
-        {/* Rendering */}
-        {scene && camera && (
-          <Composer>
-            <RenderPass scene={scene} camera={camera} />
-          </Composer>
-        )}
-        {/* Event handling */}
-        {scene && camera && <EventHandling scene={scene} camera={camera} />}
+    <Application>
+      {/* Rendering */}
+      {scene && camera && (
+        <Composer>
+          <RenderPass scene={scene} camera={camera} />
+        </Composer>
+      )}
+      {/* Event handling */}
+      {scene && camera && <EventHandling scene={scene} camera={camera} />}
 
-        {/* Camera resizing */}
-        {camera && (
-          <OnWindowResize>
-            {() => {
-              const width = window.innerWidth
-              const height = window.innerHeight
+      {/* Camera resizing */}
+      {camera && (
+        <OnWindowResize>
+          {() => {
+            const width = window.innerWidth
+            const height = window.innerHeight
 
-              if (camera instanceof THREE.PerspectiveCamera) {
-                camera.aspect = width / height
-                camera.updateProjectionMatrix()
-              }
-            }}
-          </OnWindowResize>
-        )}
+            if (camera instanceof THREE.PerspectiveCamera) {
+              camera.aspect = width / height
+              camera.updateProjectionMatrix()
+            }
+          }}
+        </OnWindowResize>
+      )}
 
-        {/* The scene, with some objects, and a camera */}
-        <T.Scene ref={setScene}>
-          <T.PerspectiveCamera position={[0, 0, 10]} ref={setCamera} />
+      {/* The scene, with some objects, and a camera */}
+      <T.Scene ref={setScene}>
+        <T.PerspectiveCamera position={[0, 0, 10]} ref={setCamera} />
 
-          <T.AmbientLight intensity={0.2} />
-          <T.DirectionalLight intensity={0.7} position={[10, 10, 10]} />
+        <T.AmbientLight intensity={0.2} />
+        <T.DirectionalLight intensity={0.7} position={[10, 10, 10]} />
 
-          <T.Mesh>
-            <T.DodecahedronGeometry />
-            <T.MeshStandardMaterial color="hotpink" />
-          </T.Mesh>
-        </T.Scene>
-      </Renderer>
-    </Ticker>
+        <T.Mesh>
+          <T.DodecahedronGeometry />
+          <T.MeshStandardMaterial color="hotpink" />
+        </T.Mesh>
+      </T.Scene>
+    </Application>
   )
 }
