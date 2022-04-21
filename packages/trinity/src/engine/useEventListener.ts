6e16ed5c5248937fcc879f5b015a047156f8649a
Hendrik Mans <hendrik@mans.de>
2022-04-21 13:15:46 +0200

    deps

--- packages/trinity/src/engine/useEventListener.ts
+++ packages/trinity/src/engine/useEventListener.ts
@@ -3,10 +3,11 @@ import { useEffect } from "react"
 export const useEventListener = <EventName extends keyof HTMLElementEventMap>(
   target: HTMLElement,
   event: EventName,
-  fn: Parameters<typeof target.addEventListener<EventName>>[1]
+  fn: Parameters<typeof target.addEventListener<EventName>>[1],
+  deps: any[] = []
 ) => {
   useEffect(() => {
     target.addEventListener(event, fn)
     return () => target.removeEventListener(event, fn)
-  }, [event, target, fn])
+  }, [event, target, fn, ...deps])
 }
