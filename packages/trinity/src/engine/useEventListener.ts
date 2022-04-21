a535817a7ef634bae505a9b0ae9e6ef6636c9ff5
Hendrik Mans <hendrik@mans.de>
2022-04-21 15:29:41 +0200

    TS 4.6.3

--- packages/trinity/src/engine/useEventListener.ts
+++ packages/trinity/src/engine/useEventListener.ts
@@ -1,13 +1,22 @@
 import { useEffect } from "react"
 
-export const useEventListener = <EventName extends keyof HTMLElementEventMap>(
-  target: HTMLElement,
-  event: EventName,
-  fn: Parameters<typeof target.addEventListener<EventName>>[1],
+export function useEventListener<
+  TElement extends HTMLElement | Document | Window,
+  TEventName extends keyof HTMLElementEventMap,
+  TEvent extends Event = HTMLElementEventMap[TEventName]
+>(
+  target: TElement,
+  event: TEventName,
+  fn: (evt: TEvent) => void,
   deps: any[] = []
-) => {
+) {
   useEffect(() => {
-    target.addEventListener(event, fn)
-    return () => target.removeEventListener(event, fn)
+    target.addEventListener(event, fn as EventListenerOrEventListenerObject)
+
+    return () =>
+      target.removeEventListener(
+        event,
+        fn as EventListenerOrEventListenerObject
+      )
   }, [event, target, fn, ...deps])
 }
