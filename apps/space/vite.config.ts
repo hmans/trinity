e9716523e649fb385b4e0ddf10f30770707584a8
Hendrik Mans <hendrik@mans.de>
2022-04-21 15:44:05 +0200

    Set things up

--- apps/space/vite.config.ts
+++ apps/space/vite.config.ts
@@ -1,7 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite"
+import react from "@vitejs/plugin-react"
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  optimizeDeps: {
+    exclude: ["react-trinity"],
+    include: ["react/jsx-runtime"]
+  },
   plugins: [react()]
 })
