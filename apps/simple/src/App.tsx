c33ed863d309bc59f936a96e13370134085cc1db
Hendrik Mans <hendrik@mans.de>
2022-04-23 16:20:22 +0200

    Play with wires

--- apps/simple/src/App.tsx
+++ apps/simple/src/App.tsx
@@ -17,41 +17,49 @@ const AutoRotate = ({ speed = 1 }) => (
   </Update>
 )
 
+function useWire<T>(initial?: T | (() => T)) {
+  const [get, set] = useState<T | null>(initial!)
+
+  return { get, set }
+}
+
 const App = () => {
-  const [scene, setScene] = useState<THREE.Scene | null>()
-  const [camera, setCamera] = useState<THREE.PerspectiveCamera | null>()
+  const scene = useWire<THREE.Scene>()
+  const camera = useWire<THREE.PerspectiveCamera>()
 
   return (
     <Ticker>
       <Renderer>
         {/* Rendering */}
-        {scene && camera && (
+        {scene.get && camera.get && (
           <Composer>
-            <RenderPass scene={scene} camera={camera} />
+            <RenderPass scene={scene.get} camera={camera.get} />
             <UnrealBloomPass />
           </Composer>
         )}
         {/* Event handling */}
-        {scene && camera && <EventHandling scene={scene} camera={camera} />}
+        {/* {scene.get && camera.get && (
+          <EventHandling scene={scene.get} camera={camera.get} />
+        )} */}
 
         {/* Camera resizing */}
-        {camera && (
+        {camera.get && (
           <OnWindowResize>
             {() => {
               const width = window.innerWidth
               const height = window.innerHeight
 
-              if (camera instanceof THREE.PerspectiveCamera) {
-                camera.aspect = width / height
-                camera.updateProjectionMatrix()
+              if (camera.get instanceof THREE.PerspectiveCamera) {
+                camera.get.aspect = width / height
+                camera.get.updateProjectionMatrix()
               }
             }}
           </OnWindowResize>
         )}
 
         {/* The scene, with some objects, and a camera */}
-        <T.Scene ref={setScene}>
-          <T.PerspectiveCamera position={[0, 0, 10]} ref={setCamera} />
+        <T.Scene ref={scene.set}>
+          <T.PerspectiveCamera position={[0, 0, 10]} ref={camera.set} />
 
           <T.AmbientLight intensity={0.2} />
           <T.DirectionalLight intensity={0.7} position={[10, 10, 10]} />
