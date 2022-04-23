9b1a3152a48d4053fa8be7c6ad0f524a12cd68cb
Hendrik Mans <hendrik@mans.de>
2022-04-23 16:39:36 +0200

    RenderPipeline

--- apps/simple/src/App.tsx
+++ apps/simple/src/App.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react"
+import { FC, useState } from "react"
 import T, {
   Composer,
   EventHandling,
@@ -17,6 +17,16 @@ const AutoRotate = ({ speed = 1 }) => (
   </Update>
 )
 
+const RenderPipeline: FC<{
+  scene: THREE.Scene
+  camera: THREE.Camera
+}> = ({ scene, camera }) => (
+  <Composer>
+    <RenderPass scene={scene} camera={camera} />
+    <UnrealBloomPass />
+  </Composer>
+)
+
 const App = () => {
   const [scene, setScene] = useState<THREE.Scene | null>()
   const [camera, setCamera] = useState<THREE.PerspectiveCamera | null>()
@@ -24,47 +34,37 @@ const App = () => {
   return (
     <Ticker>
       <Renderer>
-        <Composer>
-          {/* Rendering */}
-          {scene && camera && (
-            <>
-              <RenderPass scene={scene} camera={camera} />
-              <UnrealBloomPass />
-            </>
-          )}
+        {scene && camera && <RenderPipeline scene={scene} camera={camera} />}
+        {scene && camera && <EventHandling scene={scene} camera={camera} />}
 
-          {/* Event handling */}
-          {scene && camera && <EventHandling scene={scene} camera={camera} />}
+        {/* Camera resizing */}
+        {camera && (
+          <OnWindowResize>
+            {() => {
+              const width = window.innerWidth
+              const height = window.innerHeight
 
-          {/* Camera resizing */}
-          {camera && (
-            <OnWindowResize>
-              {() => {
-                const width = window.innerWidth
-                const height = window.innerHeight
+              if (camera instanceof THREE.PerspectiveCamera) {
+                camera.aspect = width / height
+                camera.updateProjectionMatrix()
+              }
+            }}
+          </OnWindowResize>
+        )}
 
-                if (camera instanceof THREE.PerspectiveCamera) {
-                  camera.aspect = width / height
-                  camera.updateProjectionMatrix()
-                }
-              }}
-            </OnWindowResize>
-          )}
+        {/* The scene, with some objects, and a camera */}
+        <T.Scene ref={setScene}>
+          <T.PerspectiveCamera position={[0, 0, 10]} ref={setCamera} />
 
-          {/* The scene, with some objects, and a camera */}
-          <T.Scene ref={setScene}>
-            <T.PerspectiveCamera position={[0, 0, 10]} ref={setCamera} />
+          <T.AmbientLight intensity={0.2} />
+          <T.DirectionalLight intensity={0.7} position={[10, 10, 10]} />
 
-            <T.AmbientLight intensity={0.2} />
-            <T.DirectionalLight intensity={0.7} position={[10, 10, 10]} />
-
-            <T.Mesh>
-              <T.DodecahedronGeometry />
-              <T.MeshStandardMaterial color="hotpink" />
-              <AutoRotate speed={1.5} />
-            </T.Mesh>
-          </T.Scene>
-        </Composer>
+          <T.Mesh>
+            <T.DodecahedronGeometry />
+            <T.MeshStandardMaterial color="hotpink" />
+            <AutoRotate speed={1.5} />
+          </T.Mesh>
+        </T.Scene>
       </Renderer>
     </Ticker>
   )
