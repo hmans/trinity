b7099a1bb798e4ec2bf90fd736379c5cc75ca019
Hendrik Mans <hendrik@mans.de>
2022-04-26 17:50:14 +0200

    Ignore more packages

--- apps/simple/vite.config.ts
+++ apps/simple/vite.config.ts
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react"
 // https://vitejs.dev/config/
 export default defineConfig({
   optimizeDeps: {
-    exclude: ["react-trinity"],
+    exclude: ["react-trinity", "miniplex", "miniplex-react"],
     include: ["react/jsx-runtime"]
   },
 
