654699a573e3089b22d03df9a80d6c5c162a2c33
Hendrik Mans <hendrik@mans.de>
2022-05-01 13:25:16 +0200

    Create Rapier world

--- apps/spacerage/src/lib/trinity-physics-3d/PhysicsWorld.tsx
+++ apps/spacerage/src/lib/trinity-physics-3d/PhysicsWorld.tsx
@@ -1,6 +1,6 @@
-import { useRef } from "react"
-import { ReactNode } from "react"
-import { FC } from "react"
+import * as RAPIER from "@dimforge/rapier3d-compat"
+import { useEffect } from "react"
+import { FC, ReactNode, useRef, useState } from "react"
 import T from "react-trinity"
 import { Group } from "three"
 
@@ -10,6 +10,12 @@ type PhysicsWorldProps = {
 
 export const PhysicsWorld: FC<PhysicsWorldProps> = ({ children }) => {
   const group = useRef<Group>(null!)
+  const [world, setWorld] = useState<RAPIER.World>()
 
-  return <T.Group>{children}</T.Group>
+  useEffect(() => {
+    setWorld(new RAPIER.World({ x: 0, y: -9.81, z: 0 }))
+    return () => setWorld(undefined)
+  })
+
+  return <T.Group ref={group}>{world && children}</T.Group>
 }
