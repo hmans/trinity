1de2d3bd46de025600f6e63a905b736298ebd189
Hendrik Mans <hendrik@mans.de>
2022-05-02 18:18:00 +0200

    Fix vite config

--- apps/spacerage/vite.config.ts
+++ apps/spacerage/vite.config.ts
@@ -1,7 +1,18 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite"
+import react from "@vitejs/plugin-react"
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react()],
+  optimizeDeps: {
+    exclude: [
+      "miniplex",
+      "miniplex-react",
+      "react-trinity",
+      "@hmans/controlfreak",
+      "@hmans/signal",
+      "@hmans/ui"
+    ],
+    include: ["react/jsx-runtime"]
+  }
 })
